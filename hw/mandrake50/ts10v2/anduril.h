27c164528d057a0691f543778fba64be5cb3698d
SiteRelEnby <125829806+SiteRelEnby@users.noreply.github.com>
2024-04-04 20:52:05 -0500

    fix build

--- hw/mandrake50/ts10v2/anduril.h
+++ hw/mandrake50/ts10v2/anduril.h
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #pragma once
 
-#include "hw/wurkkos/ts10/anduril.h"
+#include "hw/wurkkos/ts10/rgbaux/anduril.h"
 
 #undef RAMP_SMOOTH_CEIL
 #undef RAMP_SMOOTH_FLOOR
@@ -15,4 +15,4 @@
 #define DEFAULT_MANUAL_MEMORY 20
 #define RAMP_DISCRETE_CEIL RAMP_SMOOTH_CEIL
 #define RAMP_DISCRETE_FLOOR RAMP_SMOOTH_FLOOR
-
+#define USE_QUICK_AUX_SWITCH
